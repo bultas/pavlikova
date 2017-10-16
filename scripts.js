abf19c8c85e3a42d11b8c4ab68dbfc3429b1752d
bultas <danielbultas@gmail.com>
2017-10-17 01:25:37 +0200

    little styling, es5 scripts compatible

--- scripts.js
+++ scripts.js
@@ -1,73 +1,49 @@
+'use strict';
 
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function later() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
 
 function getPhotosContainers(sources, geometryBoxes) {
     return geometryBoxes.map(function (box, index) {
-        return `
-            <figure
-                class="gallery-box"
-                style="width: ${box.width}px; height: ${box.height}px; top: ${box.top}px; left: ${box.left}px"
-                itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject"
-            >
-                <a
-                    href="${sources[index].src}"
-                    data-size="${sources[index].width}x${sources[index].height}"
-                    itemprop="contentUrl"
-                >
-                    <img
-                        src="${sources[index].src}"
-                        width="${box.width}"
-                        height="${box.height}"
-                        itemprop="thumbnail"
-                    />
-                </a>
-            </figure>
-        `
-    }).join('\n')
+        return '\n            <figure\n                class="gallery-box"\n                style="width: ' + box.width + 'px; height: ' + box.height + 'px; top: ' + box.top + 'px; left: ' + box.left + 'px"\n                itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject"\n            >\n                <a\n                    href="' + sources[index].src + '"\n                    data-size="' + sources[index].width + 'x' + sources[index].height + '"\n                    itemprop="contentUrl"\n                >\n                    <img\n                        src="' + sources[index].src + '"\n                        width="' + box.width + '"\n                        height="' + box.height + '"\n                        itemprop="thumbnail"\n                    />\n                </a>\n            </figure>\n        ';
+    }).join('\n');
 }
 
 function getRatios(images) {
-    return images.map(function(image) {
-        return (image.width /2) / (image.height/2);
-    })
+    return images.map(function (image) {
+        return image.width / 2 / (image.height / 2);
+    });
 }
 
 function getContainerWidth(elem, maxWidth) {
-    const currentWidth = elem.offsetWidth;
+    var currentWidth = elem.offsetWidth;
     if (!maxWidth) return currentWidth;
-    return (currentWidth < maxWidth) ? currentWidth : maxWidth;
+    return currentWidth < maxWidth ? currentWidth : maxWidth;
 }
 
 function prepareGalleryLayout(sources, ratios, elem, containerWidth) {
     var justifiedLayout = require('justified-layout');
 
-    var galleryGeometry = justifiedLayout(
-        ratios,
-        {
-            containerWidth: containerWidth,
-            containerPadding: 0
-        }
-    );
+    var galleryGeometry = justifiedLayout(ratios, {
+        containerWidth: containerWidth,
+        containerPadding: 0
+    });
 
-    var boxes = getPhotosContainers(
-        sources,
-        galleryGeometry.boxes
-    )
+    var boxes = getPhotosContainers(sources, galleryGeometry.boxes);
 
     elem.innerHTML = boxes;
     elem.style.height = galleryGeometry.containerHeight + "px";
-
 }
